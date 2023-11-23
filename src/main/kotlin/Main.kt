804e6f6608acbc31876632fc6460305e87051d87
Musa <79163385+musayuksel@users.noreply.github.com>
2023-11-23 20:36:33 +0000

    var vs val

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
 fun main(args: Array<String>) {
     var myFirstVariable: String = "Hello World"//camelCase is the standard for naming variables
+    //var is a mutable variable. If you want to make it immutable, use val
+    val myFirstVariable2: String = "Hello World"
+    //myFirstVariable2 = "Hello World 2" //This will throw an error because myFirstVariable2 is immutable
     println("myFirstVariable is $myFirstVariable") // $ is called string interpolation
 }
