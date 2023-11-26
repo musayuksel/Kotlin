06ab260a1b606e1129ab587329e86bed71de0587
Musa <79163385+musayuksel@users.noreply.github.com>
2023-11-26 12:45:28 +0000

    Add exercise 7

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -82,3 +82,37 @@ fun subtract(firstNumber: Int, secondNumber: Int): Int {
 fun add(num1: Int, num2: Int): Int {
     return num1 + num2
 }
+
+//7. Default parameters
+fun displayMessage() {
+    val operatingSystem = "Chrome OS"
+    val emailId = "sample@gmail.com"
+
+    println(displayAlertMessage(operatingSystem, emailId))
+
+}
+
+fun displayMessageStep2() {
+    val firstUserEmailId = "user_one@gmail.com"
+
+    // The following line of code assumes that you named your parameter as emailId.
+    // If you named it differently, feel free to update the name.
+    println(displayAlertMessage(emailId = firstUserEmailId))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
+}
+
+fun displayAlertMessage(operatingSystem: String = "Unknown OS", emailId: String): String {
+    return "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
+}
