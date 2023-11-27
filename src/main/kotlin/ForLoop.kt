e9002b2a8c68dbee413f3eb676c572677917d053
Musa <79163385+musayuksel@users.noreply.github.com>
2023-11-27 11:55:55 +0000

    Mutable vs immutable list

--- src/main/kotlin/ForLoop.kt
+++ src/main/kotlin/ForLoop.kt
@@ -1,13 +1,21 @@
 data class User(val name: String, val age: Int)
 
-val users = listOf(
+//val users = listOf(// immutable list
+val users = mutableListOf<User>( // mutable list
     User("John", 20),
     User("Jane", 30),
     User("Mary", 40)
 )
 
 fun forLoopExample() {
+    users.add(User("Mark", 50))
     for (user in users) {
         println(user.name)
+    }
+}
+
+fun whiteAlphabet() {
+    for (letter in 'a'..'z') {
+        println(letter)
     }
 }
