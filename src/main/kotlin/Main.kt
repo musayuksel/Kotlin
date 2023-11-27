ce702f0db25ead224b72a5995937026bd465c654
Musa <79163385+musayuksel@users.noreply.github.com>
2023-11-27 10:58:02 +0000

    Move data types into a seperate file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,4 @@
 fun main() {
-    var myFirstVariable: String = "Hello World"//camelCase is the standard for naming variables
-    //var is a mutable variable. If you want to make it immutable, use val
-    val myFirstVariable2: String = "Hello World"
-    //myFirstVariable2 = "Hello World 2" //This will throw an error because myFirstVariable2 is immutable
-    println("myFirstVariable is $myFirstVariable") // $ is called string interpolation
-    //OTHER DATA TYPES
-    val myInt: Int = 1
-    val myDouble: Double = 1.0
-    val myFloat: Float = 1.0f
-    val myBoolean: Boolean = true
-    val myChar: Char = 'a'
-    val myString: String = "Hello World"
-    //ARRAYS
-    val myArray: Array<Any> = arrayOf(myInt, myDouble, myFloat, myBoolean, myChar, myString)
-    println("myArray[0] is ${myArray[0]}")
+
 }
+//Data types
