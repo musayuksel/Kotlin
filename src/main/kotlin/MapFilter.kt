5521da577d758c11e90769fc6febee1054c983f3
Musa <79163385+musayuksel@users.noreply.github.com>
2023-11-28 11:19:18 +0000

    Add custom Lambda

--- src/main/kotlin/MapFilter.kt
+++ src/main/kotlin/MapFilter.kt
@@ -12,3 +12,17 @@ fun filterSortAndUpdateUsers() {
         .map { it.copy(name = it.name.uppercase()) }
         .forEach { println("user name is ${it.name} and age is ${it.age}") }
 }
+
+//if want to create a custom filter function
+fun List<User>.customFilter(filterFunction: (User) -> Boolean): List<User> {
+    val resultList = mutableListOf<User>()
+    for (user in this) {
+        if (filterFunction(user)) {
+            resultList.add(user)
+        }
+    }
+    return resultList
+}
+// in the code above we created a custom filter function that takes a function as an argument
+// this function takes a user and returns a boolean
+// we can use this function like this
