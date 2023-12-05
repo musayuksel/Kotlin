dc4b5efed9fef566cf9e5e82d3c224e4534f2c34
Musa <79163385+musayuksel@users.noreply.github.com>
2023-12-05 16:40:18 +0000

    change file directory

--- src/main/kotlin/practices/Unit1Exercises.kt
+++ src/main/kotlin/practices/Unit1Exercises.kt
@@ -1,4 +1,4 @@
-
+package practices
 // Exercises : https://developer.android.com/codelabs/basic-android-kotlin-compose-intro-kotlin-practice-problems?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-1-pathway-1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-intro-kotlin-practice-problems#1
 //2. Print messages
 fun printMessageWithSeparatedLines() {
